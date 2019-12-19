991d6f4ac7036fd255d21fcdf92d91f81486d60a
hadolfov <55640049+hadolfov@users.noreply.github.com>
2019-12-19 09:54:39 -0600

    dos

--- src/proyecto2/pkg1/Proyecto21.java
+++ src/proyecto2/pkg1/Proyecto21.java
@@ -18,14 +18,31 @@ public class Proyecto21 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
         Scanner lector = new Scanner(System.in);
         String tecla;
-        persona p = new persona();
+        //persona p =  new persona();
         do {
-            p.registrar();
+            //p.registrar();
+
+            System.out.print("Digite el nombre: ");
+            String nombre = lector.next();
+            System.out.print("Digite el apellido: ");
+            String apellido = lector.next();
+            System.out.print("Digite la edad: ");
+            String edad = lector.next();
+            System.out.print("Digite el sexo: ");
+            String sexo = lector.next();
+            
+            persona p =  new persona(nombre,apellido,edad,sexo);
+            
             p.imprimirDatos();
             System.out.print("Desea salir? S/N: ");
             tecla = lector.next().substring(0, 1).toUpperCase();
         } while (!"S".equals(tecla));
     } 
+
+    public Proyecto21() {
+    }
 }
